ad2b54a94279ba9a93c43995d297d6dfa00ab2d4
Justin LeFurjah <lefurjah@gmail.com>
2016-01-07 16:47:39 -0600

    display all users page (logged in)

--- test/controllers/users_controller_test.rb
+++ test/controllers/users_controller_test.rb
@@ -7,6 +7,12 @@ class UsersControllerTest < ActionController::TestCase
     @other_user = users(:not_justin)
   end
 
+  test "should redirect index when not logged in" do
+    get :index
+    assert_redirected_to login_url
+  end
+
+
 
   test "should get new" do
     get :new
