3c618414aed1ce0fd49b530cf11b87f18b6eb53d
Justin LeFurjah <lefurjah@gmail.com>
2016-01-07 14:53:26 -0600

    sessions now w/long term remembering and tests

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -9,7 +9,9 @@ class UsersController < ApplicationController
 
   def create
     @user = User.new(user_params)
+
     if @user.save
+      log_in @user
       flash[:success] = "Welcome to grievr. Your account has been created."
       redirect_to @user
     else
