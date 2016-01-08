a1e53abc6df66a70f1c51f986e2517684b866c25
Justin LeFurjah <lefurjah@gmail.com>
2016-01-08 14:21:34 -0600

    account activations wrkin

--- app/controllers/account_activations_controller.rb
+++ app/controllers/account_activations_controller.rb
@@ -1,2 +1,18 @@
 class AccountActivationsController < ApplicationController
+
+  def edit
+      user = User.find_by(email: params[:email])
+      if user && !user.activated? && user.authenticated?(:activation, params[:id])
+        user.update_attribute(:activated,    true)
+        user.update_attribute(:activated_at, true)
+        user.update_attribute(:activated,    true)
+        user.update_attribute(:activated_at, Time.zone.now)
+        log_in user
+        flash[:success] = "Success! Your grievr account is set up."
+        redirect_to user
+      else
+        flash[:danger] = "Invalid activation link"
+        redirect_to root_url
+      end
+  end
 end
--- config/environments/development.rb
+++ config/environments/development.rb
@@ -41,6 +41,5 @@ Rails.application.configure do
 
   config.action_mailer.raise_delivery_errors = true
   config.action_mailer.delivery_method = :test
-  host = 'example.com'
-  config.action_mailer.default_url_options = { host: 'localhost:3000' } 
+  config.action_mailer.default_url_options = { host: 'localhost:3000' }
 end
