3b6a0d26c2e4ee5116b17d65f060cb1f4aef45e9
Justin LeFurjah <lefurjah@gmail.com>
2016-01-11 14:25:01 -0600

    user activation is dunnnn

--- app/controllers/account_activations_controller.rb
+++ app/controllers/account_activations_controller.rb
@@ -3,10 +3,7 @@ class AccountActivationsController < ApplicationController
   def edit
       user = User.find_by(email: params[:email])
       if user && !user.activated? && user.authenticated?(:activation, params[:id])
-        user.update_attribute(:activated,    true)
-        user.update_attribute(:activated_at, true)
-        user.update_attribute(:activated,    true)
-        user.update_attribute(:activated_at, Time.zone.now)
+        user.activate
         log_in user
         flash[:success] = "Success! Your grievr account is set up."
         redirect_to user
